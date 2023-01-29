a2c7909c179d7419f5312b6c9ee8cf484204a1a1
Alfred Obialo <alfredcsdinc@gmail.com>
2023-01-29 22:03:58 +0100

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component , VERSION} from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'rented';
+  version :string  = VERSION.full;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {provideHttpClient} from "@angular/common/http";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideRouter} from "@angular/router";
 
-import { AppModule } from './app/app.module';
 
+bootstrapApplication(AppComponent, {
+  providers : [provideHttpClient(), provideAnimations(), provideRouter([])]
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+}).then(x => {
+
+})
